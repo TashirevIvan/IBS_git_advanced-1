ed37c37c59f0192e5679a16ad1f83ba90de79620
TashirevIvan <tashirev.ivan@yandex.ru>
2025-05-26 14:18:41 +0300

    add initial code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 99; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer!");
+            System.out.println("Take one down, pass it around");
+            System.out.println((i - 1) + " bottles of beer on the wall!\n");
         }
+        System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
+        System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall...");
     }
 }
